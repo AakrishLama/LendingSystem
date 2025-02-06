8e7ec69fce45ff2bb494890665efbfeab5dbe03c
Aakrish Lama <al226na@student.lnu.se>
2025-02-06 17:19:40 +0100

    preparing for routes

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Signup from './Signup'
+import Login from './Login'
 
 export default function Home() {
   return (
     <div>
       <div><Signup/></div>
+      <div><Login/></div>
     </div>
   )
 }
