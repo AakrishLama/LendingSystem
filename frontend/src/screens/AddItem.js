f39eb345effb72347f2e3cb49791582aa7656c48
Aakrish Lama <al226na@student.lnu.se>
2025-02-19 20:52:04 +0100

    add item without image complete

--- frontend/src/screens/AddItem.js
+++ frontend/src/screens/AddItem.js
@@ -4,8 +4,6 @@ import { useState, useContext } from "react"
 import AuthContext from '../components/AuthContext'
 
 
-// i need current user ID, Token.
-
 export default function AddItem() {
   const [item, setItem] = useState({ name: "", description: "", pricePerDay: 10, category: "", available: true })
   const { user, token } = useContext(AuthContext);
@@ -21,19 +19,27 @@ export default function AddItem() {
     console.log("User ID:", user.id);
     console.log("user", user);
     console.log("item", item);
+
+    // check if any field is empty
+    if (item.category === "" || item.name === "" || item.description === "" || item.pricePerDay === "" || item.available === "") {
+      alert(" Please fill all the fields");
+      return
+    }
     try {
       const response = await fetch(`http://localhost:8080/itemContract/addItem/${user.id}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization":`Bearer ${token}`
+          "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify(item)
       });
       if (response.status === 200) {
+        // return the added item.
         const data = await response.json();
         console.log(JSON.stringify(data));
         alert("Item added successfully");
+        // reset the form
         setItem({ name: "", description: "", pricePerDay: 10, category: "", available: true })
       }
     }
@@ -58,10 +64,11 @@ export default function AddItem() {
           </div>
           <div className="col-auto my-1">
             <label className="mr-sm-2 me-2" htmlFor="inlineFormCustomSelect">Category</label>
-            <select className="custom-select mr-sm-2" id="inlineFormCustomSelect" value={item.category} name="category" onChange={handleChange}>category
+            <select className="custom-select mr-sm-2" id="inlineFormCustomSelect" value={item.category} name="category" onChange={handleChange}>
+              <option value="">Choose</option>
               <option value="TOOL">TOOL</option>
               <option value="SHOE">SHOE</option>
-              <option value="Book">Book</option>
+              <option value="BOOK">BOOK</option>
               <option value="VEHICLE">VEHICLE</option>
               <option value="TOY">TOY</option>
               <option value="SPORT">SPORT</option>
@@ -74,11 +81,15 @@ export default function AddItem() {
           </div>
 
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" id="inlineRadio1" name="available" value={item.available = true} onChange={handleChange} />
+            <input className="form-check-input" type="radio" id="inlineRadio1" name="available" checked={item.available === true} // Ensure it's checked properly
+              value={item.available} onChange={() => setItem({ ...item, available: true })
+              } />
             <label className="form-check-label" htmlFor="inlineRadio1">Available</label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" id="inlineRadio2" name="available" value={item.available = false} onChange={handleChange} />
+            <input className="form-check-input" type="radio" id="inlineRadio2" name="available" checked={item.available === false} // Ensure it's checked properly
+              value={item.available} onChange={() => setItem({ ...item, available: false })
+              } />
             <label className="form-check-label" htmlFor="inlineRadio2">Not Available</label>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
